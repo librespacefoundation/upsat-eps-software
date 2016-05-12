f7bcd1ca3fb5a6ddcdae580b5ac7047e519dfac1
nchronas <nchronas@gmail.com>
2016-05-12 07:55:23 -0700

    integration for ecss services.

--- epsSubsystemWorkspace/epsSubsystem/Src/stm32l1xx_it.c
+++ epsSubsystemWorkspace/epsSubsystem/Src/stm32l1xx_it.c
@@ -181,7 +181,7 @@ void USART1_IRQHandler(void)
 */
 void USART3_IRQHandler(void) {
 	/* USER CODE BEGIN USART3_IRQn 0 */
-
+	HAL_EPS_UART_IRQHandler(&huart3);
 	/* USER CODE END USART3_IRQn 0 */
 	HAL_UART_IRQHandler(&huart3);
 	/* USER CODE BEGIN USART3_IRQn 1 */
